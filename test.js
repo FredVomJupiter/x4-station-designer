9c8f009a39662e9728467900ab6fdb2a334a7faa
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-07 14:28:00 +0300

    added list of modules with container & icons

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ function getModules() {
     for (let index = 0; index < stationModules.modules.length; index++) {
         let moduleField = document.getElementById('modulesSelection');
         moduleField.innerHTML += `<option value="${stationModules.modules[index].name}">${stationModules.modules[index].name}</option>`;
-        
+
     }
 }
