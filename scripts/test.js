43f7e00e5af3dcae5caf3eff9e1b5b951ede7e5c
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-07 14:50:52 +0300

    implemented filter for module list btns

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,80 @@
-function getModules() {
+function getAllModules() {
+    clearModuleList();
+    let moduleField = document.getElementById('modulesSelection');
     for (let index = 0; index < stationModules.modules.length; index++) {
-        let moduleField = document.getElementById('modulesSelection');
         moduleField.innerHTML += `<option value="${stationModules.modules[index].name}">${stationModules.modules[index].name}</option>`;
 
     }
+}
+
+
+function getEnergyModules() {
+    clearModuleList();
+    let moduleField = document.getElementById('modulesSelection');
+    for (let index = 0; index < stationModules.modules.length; index++) {
+        if (stationModules.modules[index].type === 'Energy') {
+            moduleField.innerHTML += `<option value="${stationModules.modules[index].name}">${stationModules.modules[index].name}</option>`;
+        }
+    }
+}
+
+
+function getRefinedModules() {
+    clearModuleList();
+    let moduleField = document.getElementById('modulesSelection');
+    for (let index = 0; index < stationModules.modules.length; index++) {
+        if (stationModules.modules[index].type === 'Refined Goods') {
+            moduleField.innerHTML += `<option value="${stationModules.modules[index].name}">${stationModules.modules[index].name}</option>`;
+        }
+    }
+}
+
+
+function getFoodModules() {
+    clearModuleList();
+    let moduleField = document.getElementById('modulesSelection');
+    for (let index = 0; index < stationModules.modules.length; index++) {
+        if (stationModules.modules[index].type === 'Food') {
+            moduleField.innerHTML += `<option value="${stationModules.modules[index].name}">${stationModules.modules[index].name}</option>`;
+        }
+    }
+}
+
+
+function getHighTechModules() {
+    clearModuleList();
+    let moduleField = document.getElementById('modulesSelection');
+    for (let index = 0; index < stationModules.modules.length; index++) {
+        if (stationModules.modules[index].type === 'High Tech') {
+            moduleField.innerHTML += `<option value="${stationModules.modules[index].name}">${stationModules.modules[index].name}</option>`;
+        }
+    }
+}
+
+
+function getMedicalModules() {
+    clearModuleList();
+    let moduleField = document.getElementById('modulesSelection');
+    for (let index = 0; index < stationModules.modules.length; index++) {
+        if (stationModules.modules[index].type === 'Medical') {
+            moduleField.innerHTML += `<option value="${stationModules.modules[index].name}">${stationModules.modules[index].name}</option>`;
+        }
+    }
+}
+
+
+function getShipTechModules() {
+    clearModuleList();
+    let moduleField = document.getElementById('modulesSelection');
+    for (let index = 0; index < stationModules.modules.length; index++) {
+        if (stationModules.modules[index].type === 'Ship Tech') {
+            moduleField.innerHTML += `<option value="${stationModules.modules[index].name}">${stationModules.modules[index].name}</option>`;
+        }
+    }
+}
+
+
+function clearModuleList() {
+    let moduleField = document.getElementById('modulesSelection');
+    moduleField.innerHTML = '';
 }
